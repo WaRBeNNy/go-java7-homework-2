ed0161eb154f7105d51c3d65984a936d42845eef
WaRBeNNy <guselnikov_s@i.ua>
2017-04-17 16:58:59 +0300

    Homework task 01.2

---
+++ src/HW02.java
@@ -0,0 +1,54 @@
+/*Сергей Гусельников
+Создать консольное приложение, которое просит пользователя ввести из консоли 5 чисел
+Все числа должны быть записаны в массив целых чисел.
+
+После этого, массив должен быть отсортирован по возрастанию и выведен консоль.
+Сначала начальная версия, далее - отсортированная.
+
+Пример:
+
+Enter number 1:
+5
+Enter number 2:
+4
+Enter number 3:
+3
+Enter number 4:
+2
+Enter number 5:
+1
+
+Initial array: 5, 4, 3, 2, 1
+Sorted array: 1, 2, 3, 4, 5.
+ */
+import java.util.Scanner;
+
+public class HW02 {
+    public static void main(String[] args) {
+        int nums[] = new int[5];
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=0;i<5;i++) {
+            System.out.println("Enter number " + (i+1) + ":");
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Initial array: ");
+        for(int i=0; i<5; i++)
+            System.out.print(nums[i] + " ");
+        System.out.println();
+
+        for(int i=4; i>0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int tmp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = tmp;
+                }
+            }
+        }
+        System.out.print("Sorted array: ");
+        for(int i=0; i<5; i++)
+            System.out.print(nums[i] + " ");
+    }
+}
